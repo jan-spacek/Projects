37b20f8c021259176b26f19116c44f02f76a2979
jan-spacek <jan.spacek@gmail.com>
2016-03-21 11:53:16 +0100

    gulpfile

---
+++ MyNurserySchool/src/MyNurserySchool/gulpfile.js
@@ -0,0 +1,10 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var ngAnotate = require('gulp-ng-annotate');
+
+gulp.task('minify', function () {
+    return gulp.src("wwwroot/app/*/*.js")
+        .pipe(ngAnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest("wwwroot/lib/_app"));
+});
