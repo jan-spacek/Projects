ef81ff8aaad3225a81e0cfd9996546c1c1ce2756
jan-spacek <jan.spacek@gmail.com>
2016-10-27 19:43:50 +0200

    Upravy

--- MyNurserySchool/src/MyNurserySchool/wwwroot/lib/_app/concat.js
+++ MyNurserySchool/src/MyNurserySchool/wwwroot/lib/_app/concat.js
@@ -868,6 +868,110 @@
 
     "use strict";
 
+    NurseryDetailController.$inject = ["$scope", "$rootScope", "$http", "$location", "$window", "$controller", "DataService"];
+    angular.module("app.nursery")
+        .controller("NurseryDetailController", NurseryDetailController);
+
+    function NurseryDetailController($scope, $rootScope, $http, $location, $window, $controller, DataService) {
+        $controller('BaseController', {
+            '$scope':$scope
+        });
+
+        var vm = this;
+        
+        vm.nursery = {};
+        vm.isBusy = true;
+        $scope.sortType = 'title';
+        $scope.sortReverse = true;
+
+        activate()
+
+        function activate() {
+            if ($rootScope.nursery != null) {
+                vm.nurseryId = $rootScope.nursery.id;
+                getNursery();
+            } else {
+                $location.path("/nurseries");
+            }
+        }
+
+        function getNursery() {
+            DataService.getNursery(vm.nurseryId)
+                .then(function (response) {
+                    angular.copy(response.data, vm.nursery);
+                    if (vm.nursery.classes.length > 0) {
+                        vm.isBusy = true;
+                        for (var i = 0; i < vm.nursery.classes.length; i++) {
+                            getClass(i);
+                        }
+                    }
+                }, function () {
+                    toastr.error("Nepodarilo sa načítať informácie o škôlke");
+                }).finally(function () {
+                    vm.isBusy = false;
+                });
+        }
+
+        function getClass(i) {
+            DataService.getClass(vm.nursery.classes[i].id)
+                .then(function (response) {
+                    for (var j = 0; j < response.data.children.length; j++)
+                        if (response.data.children[j].attendance == 1)
+                            vm.nursery.classes[i].children.push(response.data.children[j]);
+                }, function () {
+                    toastr.error("Nepodarilo sa načítať informácie o triede");
+                });
+        }
+    }
+})(angular);
+(function (angular) {
+    "use strict";
+
+    NurseryEditController.$inject = ["$scope", "$http", "$location", "$controller", "$rootScope", "DataService"];
+    angular.module("app.nursery")
+        .controller("NurseryEditController", NurseryEditController);
+
+    function NurseryEditController($scope, $http, $location, $controller, $rootScope, DataService) {
+        $controller('BaseController', {
+            '$scope':$scope
+        });
+
+        var vm = this;
+
+        vm.nurseryId = $rootScope.nursery.id;
+        vm.nursery = {};
+        vm.isBusy = true;
+
+        DataService.getNursery(vm.nurseryId)
+            .then(function (response) {
+                angular.copy(response.data, vm.nursery);
+            }, function () {
+                toastr.error("Nepodarilo sa načítať informácie o škôlke");
+            }).finally(function () {
+                vm.isBusy = false;
+            });
+
+        vm.saveNursery = function (isValid) {
+            if (isValid) {
+                vm.isBusy = true;
+
+                DataService.updateNursery(vm.nursery)
+                    .then(function (response) {
+                        toastr.success("Zmeny v škôlke " + vm.nursery.name + " boli úspešne uložené");
+                        $location.path("#/");
+                    }, function () {
+                        toastr.error("Škôlku sa nepodarilo uložiť");
+                    }).finally(function () {
+                        vm.isBusy = false;
+                    });
+            }
+        }
+    }
+})(angular);
+(function (angular) {
+
+    "use strict";
+
     NurseriesListController.$inject = ["$http", "$scope", "$window", "$location", "$rootScope", "DataService"];
     angular.module("app.nursery")
         .controller("NurseriesListController", NurseriesListController);
@@ -980,110 +1084,6 @@
                 .finally(function () {
                     vm.isBusy = false;
                 });
-        }
-    }
-})(angular);
-(function (angular) {
-
-    "use strict";
-
-    NurseryDetailController.$inject = ["$scope", "$rootScope", "$http", "$location", "$window", "$controller", "DataService"];
-    angular.module("app.nursery")
-        .controller("NurseryDetailController", NurseryDetailController);
-
-    function NurseryDetailController($scope, $rootScope, $http, $location, $window, $controller, DataService) {
-        $controller('BaseController', {
-            '$scope':$scope
-        });
-
-        var vm = this;
-        
-        vm.nursery = {};
-        vm.isBusy = true;
-        $scope.sortType = 'title';
-        $scope.sortReverse = true;
-
-        activate()
-
-        function activate() {
-            if ($rootScope.nursery != null) {
-                vm.nurseryId = $rootScope.nursery.id;
-                getNursery();
-            } else {
-                $location.path("/nurseries");
-            }
-        }
-
-        function getNursery() {
-            DataService.getNursery(vm.nurseryId)
-                .then(function (response) {
-                    angular.copy(response.data, vm.nursery);
-                    if (vm.nursery.classes.length > 0) {
-                        vm.isBusy = true;
-                        for (var i = 0; i < vm.nursery.classes.length; i++) {
-                            getClass(i);
-                        }
-                    }
-                }, function () {
-                    toastr.error("Nepodarilo sa načítať informácie o škôlke");
-                }).finally(function () {
-                    vm.isBusy = false;
-                });
-        }
-
-        function getClass(i) {
-            DataService.getClass(vm.nursery.classes[i].id)
-                .then(function (response) {
-                    for (var j = 0; j < response.data.children.length; j++)
-                        if (response.data.children[j].attendance == 1)
-                            vm.nursery.classes[i].children.push(response.data.children[j]);
-                }, function () {
-                    toastr.error("Nepodarilo sa načítať informácie o triede");
-                });
-        }
-    }
-})(angular);
-(function (angular) {
-    "use strict";
-
-    NurseryEditController.$inject = ["$scope", "$http", "$location", "$controller", "$rootScope", "DataService"];
-    angular.module("app.nursery")
-        .controller("NurseryEditController", NurseryEditController);
-
-    function NurseryEditController($scope, $http, $location, $controller, $rootScope, DataService) {
-        $controller('BaseController', {
-            '$scope':$scope
-        });
-
-        var vm = this;
-
-        vm.nurseryId = $rootScope.nursery.id;
-        vm.nursery = {};
-        vm.isBusy = true;
-
-        DataService.getNursery(vm.nurseryId)
-            .then(function (response) {
-                angular.copy(response.data, vm.nursery);
-            }, function () {
-                toastr.error("Nepodarilo sa načítať informácie o škôlke");
-            }).finally(function () {
-                vm.isBusy = false;
-            });
-
-        vm.saveNursery = function (isValid) {
-            if (isValid) {
-                vm.isBusy = true;
-
-                DataService.updateNursery(vm.nursery)
-                    .then(function (response) {
-                        toastr.success("Zmeny v škôlke " + vm.nursery.name + " boli úspešne uložené");
-                        $location.path("#/");
-                    }, function () {
-                        toastr.error("Škôlku sa nepodarilo uložiť");
-                    }).finally(function () {
-                        vm.isBusy = false;
-                    });
-            }
         }
     }
 })(angular);
